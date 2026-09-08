48f40dc11d0b9f9a9eb3444978a787b00266ba7d
JoelKent85 <jkent06@calpoly.edu>
2026-09-07 21:08:39 -0700

    fixed allow_origins

--- backend/main.py
+++ backend/main.py
@@ -55,7 +55,7 @@ class InputData(BaseModel):
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://codebox-hackathon-project-1.onrender.com/"],
+    allow_origins=["https://codebox-hackathon-project-1.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
